01f7f060c2bb81caeeccbc2407e479e595294c67
Mat08251 <duguet@simplon-charleville.fr>
2020-04-29 17:40:00 +0200

    test-de-connexion

--- traitement.php
+++ traitement.php
@@ -8,13 +8,23 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
+$pseudo = htmlspecialchars($_POST['pseudo']);
+$pseudo = htmlspecialchars($_POST['pass']);
+$pseudo = htmlspecialchars($_POST['email']);
+$pseudo = htmlspecialchars($_POST['nom']);
+
+if(!empty($_POST['pseudo']) AND !empty($_POST['pass']) AND !empty($_POST['email']) AND !empty($_POST['nom']))
+{
+
+
+
 $req = $bdd->prepare('INSERT INTO user(pseudo, pass, email, nom) VALUES(:pseudo, :pass, :email, :nom)');
 $req->execute(array(
-    'pseudo' => $pseudo,
-    'pass' => $pass,
-    'email' => $email,
-    'nom' => $nom     
+    'pseudo' => $_POST['pseudo'],
+    'pass' => $_POST['pass'],
+    'email' => $_POST['email'],
+    'nom' => $_POST['nom']     
 ));
 
-echo 'vous vous êtes bien inscrit';
+
 ?>
